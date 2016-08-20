da74a9a844c4edc1cebc53995bb9b5264ecdf8b4
Byron Mejia <byron.mejia@connect.qut.edu.au>
2016-08-20 19:08:05 +1000

    Pray for Logins

--- app/models/login.js
+++ app/models/login.js
@@ -1,6 +1,22 @@
+const bcrypt = require('bcrypt');
+const Promise = require('bluebird');
+
 module.exports = (bookshelf) => {
-  // noinspection Eslint
+  /* eslint-disable */
   const Login = bookshelf.Model.extend({
     tableName: 'logins',
+    initialize: () => {
+      this.on('creating', this.hashPassword, this);
+    },
+    hashPassword: (model) => new Promise(
+      (resolve, reject) => bcrypt.hash(
+        model.attributes.password, 10, (err, hash) => {
+          if (err) reject(err);
+          model.set('password', hash);
+          resolve(hash);
+        }
+      )
+    ),
   });
+  /* eslint-enable */
 };
