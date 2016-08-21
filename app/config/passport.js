68fb97fa04c0780368afd4f82b0096eb5d48195c
Byron Mejia <byron.mejia@connect.qut.edu.au>
2016-08-22 00:03:18 +1000

    Login Complete

--- app/config/passport.js
+++ app/config/passport.js
@@ -1,19 +1,24 @@
 const path = require('path');
+const bcrypt = require('bcrypt');
 const Strategy = require('passport-local').Strategy;
 const Login = require(path.join(__dirname, '..', 'models', 'login.js'));
 
-module.exports = function(passport) {
+const isValidPassword = function (user, password) {
+  return bcrypt.compareSync(password, user.attributes.password);
+};
+
+module.exports = function (passport) {
   passport.use('local', new Strategy({
     usernameField: 'username',
     passwordField: 'password',
   },
   function (username, password, cb) {
-    console.log('Doing Strategy');
     Login
-      .where({ username: username })
+      .where({ username })
       .fetch()
       .then((data) => {
-        console.log(data);
+        if (!data.attributes.username) return cb(null, false);
+        if (!isValidPassword(data, password)) return cb(null, false);
         return cb(null, data);
       });
     }
--- app/routes/auth.js
+++ app/routes/auth.js
@@ -11,11 +11,22 @@ module.exports = (app, passport) => {
     }
   });
 
-  app.post('/login', passport.authenticate('local', {
-    failureRedirect: '/login',
-    successRedirect: '/',
-    session: false,
-  }));
+  app.post('/login', (req, res) => {
+    passport.authenticate('local',
+      (err, user) => {
+        if (err) return res.redirect('/error?id=1');
+        if (!user) return res.redirect('/login?attempt=1');
+        let good = true;
+        req.login(user, () => {
+          if (err){
+            return res.redirect('/error?id=3');
+          } else {
+            return res.redirect('/success');
+          }
+        });
+      }
+    )(req, res);
+  });
 
   app.get('/logout', (req, res) => {
     res.cookie('authToken', {});
