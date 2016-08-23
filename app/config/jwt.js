31e7348ff9cb47d73108f02470e1ce960d8f2e96
Byron Mejia <byron.mejia@connect.qut.edu.au>
2016-08-24 00:58:38 +1000

    Build on the fly

--- app/config/jwt.js
+++ app/config/jwt.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const jwt = require('jwt-simple');
-const secret = process.env.secret_key || fs.readFileSync(
+const secret = process.env.SECRET_KEY || fs.readFileSync(
   path.join(__dirname, 'secret.key'),
   'utf8'
 );
--- heroku/bootstrap.sh
+++ heroku/bootstrap.sh
@@ -1,3 +1,4 @@
 node heroku/keygen.js
 knex migrate:latest
 knex seed:run development
+heroku config:set SECRET_KEY=$( cat app/config/secret.key )
