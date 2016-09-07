7aa8e32642bbbf4cc63e7c77f46d915b0b56bf5b
jezekab <jezekab@gmail.com>
2016-09-07 10:09:38 +1000

    node doesn't like dashes

--- app/routes/create-event.js
+++ app/routes/create-event.js
@@ -1,3 +1,14 @@
 module.exports = (app, passport, jwt, jwtAuth) => {
-  app.get('/events/create', jwtAuth, (req, res) => res.render('create-event'));
+  app.get('/events/create', jwtAuth, (req, res) => res.render('create_event'));
 };
+
+/* app.post('/events/create', jwtAuth, (req, res) => {
+      req.body.event_name
+      req.body.event_desc
+      req.body.event_location
+      req.body.event_startdate
+      req.body.event_starttime
+      req.body.event_enddate
+      req.body.event_endtime
+  });
+}; */
