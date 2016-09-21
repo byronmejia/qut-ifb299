74ef6dc814cef3c6ceb9acda641e9fdbca270fa5
jezekab <jezekab@gmail.com>
2016-09-21 16:35:36 +1000

    pls

--- app/routes/signup.js
+++ app/routes/signup.js
@@ -13,23 +13,16 @@ module.exports = (app) => {
     new Login({
       username: req.body.username,
       password: req.body.password,
-    }).save().then(() => {
-      Login.where('username', req.body.username).fetch({
-        require: true,
-        columns: id,
-      }).then((resData) => {
-        var data = JSON.parse(JSON.stringify(resData));
-        console.log(data);
-        new Profile({
-          firstName: req.body.first_name,
-          lastName: req.body.last_name,
-          email: req.body.email,
-          mobile: req.body.mobile,
-          bio: req.body.bio,
-          login_id: data,
-        }).save().then(() => {
-          res.send('Thanks for signing up');
-        });
+    }).save().then((login) => {
+      new Profile({
+        firstName: req.body.first_name,
+        lastName: req.body.last_name,
+        email: req.body.email,
+        mobile: req.body.mobile,
+        bio: req.body.bio,
+        login_id: login.attributes.id,
+      }).save().then(() => {
+        res.send('Thanks for signing up');
       });
     });
   });
