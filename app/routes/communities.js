44053f8aee78ac77392b1c9dd90f7efa6527d733
Byron Mejia <byron.mejia@connect.qut.edu.au>
2016-09-22 13:24:25 +1000

    Stripe page nearly working, thick boy 🎉

--- app/routes/communities.js
+++ app/routes/communities.js
@@ -5,7 +5,29 @@ const path = require('path');
 
 const Communities = require(path.join('..', 'models', 'Community.js'));
 
-module.exports = (app, passport, jwt, jwtAuth) => {
+const keyPath = path.join('..', 'config', 'keys.json');
+
+let keys;
+
+try {
+  keys = require(keyPath); // eslint-disable-line
+} catch (err) {
+  console.log(err); // eslint-disable-line no-console
+  keys = {
+    stripe: {
+      test: {
+        publish: process.env.ST_TEST_PUB,
+        secret: process.env.ST_TEST_SEC,
+      },
+      live: {
+        publish: process.env.ST_LIVE_PUB,
+        secret: process.env.ST_LIVE_SEC,
+      },
+    },
+  };
+}
+
+module.exports = (app, passport, jwt, jwtAuth, opts) => {
   app.get('/communities', jwtAuth, (req, res) => {
     res.render('app/communities');
   });
@@ -19,11 +41,14 @@ module.exports = (app, passport, jwt, jwtAuth) => {
   app.get('/communities/:id/donate', jwtAuth, (req, res) => {
     Communities.where({ id: req.params.id }).fetch()
       .then((data) => {
-        res.render('app/communities/donate', { community: data });
+        res.render(
+          'app/communities/donate', { community: data, stripe_pub: keys.stripe.test.publish }
+          );
       });
   });
 
   app.post('/communities/:id/donate', jwtAuth, (req, res) => {
-    // Do something
+    console.log(req.body.stripeToken);
+    console.log(req.body.finalAmount);
   });
 };
