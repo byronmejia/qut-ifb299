eea7eaf725a9b10ce4ce18f5859b65ebfbf4d4b4
jezekab <jezekab@gmail.com>
2016-09-22 14:58:03 +1000

    Feature/communities (#30)
    * i don't even know anymore
    
    * editing of communities works

--- app/routes/communities.js
+++ app/routes/communities.js
@@ -4,14 +4,62 @@
 const path = require('path');
 
 const Communities = require(path.join('..', 'models', 'Community.js'));
+const Relationship = require(
+  path.join(__dirname, '..', 'models', 'RelationshipProfileCommunity.js'));
 
 module.exports = (app, passport, jwt, jwtAuth) => {
   app.get('/communities/create', jwtAuth, (req, res) => res.render('create-community'));
-};
 
-module.exports = (app, passport, jwt, jwtAuth) => {
   app.get('/communities', jwtAuth, (req, res) => {
     res.render('app/communities');
+  });
+
+  app.get('/community/:id', jwtAuth, (req, res) => {
+    Communities.where({
+      id: req.params.id,
+    }).fetch({
+      require: true,
+    }).then((data) => {
+      res.render('app/community-page', {
+        community: data.attributes,
+      });
+    });
+  });
+
+  app.post('/community/:id/join', jwtAuth, (req, res) => {
+    new Relationship({
+      community_id: req.params.id,
+      profile_id: 1111,
+    }).save().then(() => {
+      res.redirect('/communities');
+    });
+  });
+
+  app.get('/community/:id/edit', jwtAuth, (req, res) => {
+    Communities.where({
+      id: req.params.id,
+    }).fetch({
+      require: true,
+    }).then((data) => {
+      res.render('edit-community', {
+        community: data.attributes,
+      });
+    });
+  });
+
+  app.post('/community/:id/edit/success', jwtAuth, (req, res) => {
+    Communities.where({
+      id: req.params.id,
+    }).save({
+      name: req.body.community_name,
+      profile_picture: null,
+      description: req.body.community_desc,
+      location: req.body.community_location,
+    }, {
+      patch: true,
+    }).then(() => {
+      res.redirect('/communities');
+    });
   });
 
   app.get('/api/communities', jwtAuth, (req, res) => {
--- app/routes/create-event.js
+++ app/routes/create-event.js
@@ -1,6 +1,8 @@
 const path = require('path');
 
 const Event = require(path.join(__dirname, '..', 'models', 'Event.js'));
+// const RelationshipRsvpEventProfile = require(
+  // path.join(__dirname, '..', 'models', 'RelationshipRsvpEventProfile.js'));
 
 module.exports = (app, passport, jwt, jwtAuth) => {
   app.get('/events/create', jwtAuth, (req, res) => res.render('create-event'));
@@ -19,3 +21,9 @@ module.exports = (app, passport, jwt, jwtAuth) => {
     });
   });
 };
+
+/*
+profile_id
+event_id
+rsvp_id
+*/
