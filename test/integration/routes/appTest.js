e7f7270c9eadb261b9e18f89e119c33d8e3b6654
Byron Mejia <byronelenilson@outlook.com.au>
2016-09-22 23:32:36 +1000

    Feature/code cleanup (#41)
    * Some more tests
    
    * Sign up form works, but is very broken
    
    * Can now create communities
    
    * Well commented code, mlady
    
    * Documented apps routes
    
    * Much Better
    
    * encryption
    
    * Dunno why no work

---
+++ test/integration/routes/appTest.js
@@ -0,0 +1,51 @@
+const expect = require('chai').expect;
+const describe = require('mocha').describe;
+const it = require('mocha').it;
+const request = require('supertest');
+const path = require('path');
+
+const app = require(
+  path.join(__dirname, '..', '..', '..', 'app', 'app.js')
+).getApp;
+
+describe('GET /profile', function() {
+  it('Fails when not authenticated', function(done) {
+    request(app)
+      .get('/profile')
+      .expect(302, done);
+  });
+
+  it('Will return 200/OK after authenticated', function(done) {
+    request(app)
+      .get('/profile')
+      .expect(302, done);
+  });
+});
+
+describe('GET /home', function() {
+  it('Fails when not authenticated', function(done) {
+    request(app)
+      .get('/home')
+      .expect(302, done);
+  });
+
+  it('Will return 200/OK after authenticated', function(done) {
+    request(app)
+      .get('/home')
+      .expect(302, done);
+  });
+});
+
+describe('GET /dashboard', function() {
+  it('Fails when not authenticated', function(done) {
+    request(app)
+      .get('/dashboard')
+      .expect(302, done);
+  });
+
+  it('Will return 200/OK after authenticated', function(done) {
+    request(app)
+      .get('/dashboard')
+      .expect(302, done);
+  });
+});
