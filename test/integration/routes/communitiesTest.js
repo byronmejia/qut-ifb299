370a29e50d5923cc669499fee4ddf2516ba1a17b
Byron Mejia <byron.mejia@connect.qut.edu.au>
2016-10-25 22:56:00 +1000

    bishes dis will break cause der is issue in communities

--- test/integration/routes/communitiesTest.js
+++ test/integration/routes/communitiesTest.js
@@ -28,19 +28,31 @@ describe('Profile Router Integration', () => {
   describe('GET', () => {
     it('Will return 200/OK at /', (done) => {
       agent
-        .get('/profile')
-        .expect(200, done);
-    });
-
-    it('Will return 200/OK at /edit', (done) => {
-      agent
-        .get('/profile/edit')
+        .get('/communities')
         .expect(200, done);
     });
 
     it('Will return 200/OK at /:id (1)', (done) => {
       agent
-        .get('/profile/1')
+        .get('/communities/1')
+        .expect(200, done);
+    });
+
+    it('Will return 200/OK at /create', (done) => {
+      agent
+        .get('/communities/create')
+        .expect(200, done);
+    });
+
+    it('Will return 200/OK at /:id/edit', (done) => {
+      agent
+        .get('/communities/1/edit')
+        .expect(200, done);
+    });
+
+    it('Will return 200/OK at /:id/donate', (done) => {
+      agent
+        .get('/communities/1/donate')
         .expect(200, done);
     });
   });
